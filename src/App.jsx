8e7dccc531aa65d19c728cb32eed72e6732b1ffb
Simone Carriero <simonecarriero95@hotmail.com>
2023-01-12 11:23:07 +0100

    Add functions for the dice, install nanoid

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,30 @@
 import React from 'react';
+import { nanoid } from 'nanoid';
 
 const App = () => {
+  /* Functions for the dice */
+
+  /* Pick a random number from 1 to 6 */
+  const randomNumber = () => {
+    return Math.ceil(Math.random() * 6);
+  };
+
+  /* Generate new dice object */
+  const generateNewDie = () => ({
+    value: randomNumber(),
+    isHeld: false,
+    id: nanoid(),
+  });
+
+  /* Create an array of 10 dice objects with a for loop */
+  const allNewDice = () => {
+    let newDice = [];
+    for (let i = 0; i < 10; i++) {
+      newDice.push(generateNewDie());
+    }
+    return newDice;
+  };
+
   return <main className='main'></main>;
 };
 
