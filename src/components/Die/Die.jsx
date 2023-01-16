bd1910d71fc9e522d7a58ca14862c815c5efbae4
Simone Carriero <simonecarriero95@hotmail.com>
2023-01-16 14:44:45 +0100

    Update Die to show dinamically image of real die

--- src/components/Die/Die.jsx
+++ src/components/Die/Die.jsx
@@ -2,9 +2,19 @@ import React from 'react';
 import './Die.css';
 
 const Die = ({ value, isHeld, holdDice }) => {
-  return <div className={`die ${isHeld && 'die--selected'}`} onClick={holdDice}>
-  {value}
-  </div>;
+  const styles = {
+    background: {
+      background: `url('dice/${value}.png') center center`,
+      backgroundSize: 'cover',
+    },
+  };
+
+  return (
+    <div
+      style={styles.background}
+      className={`die ${isHeld && 'die--selected'}`}
+      onClick={holdDice}></div>
+  );
 };
 
 export default Die;
